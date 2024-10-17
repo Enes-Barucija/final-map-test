570b4b52321c70b6440f1b3a2ab4a39ce8ae24fa
Enes-Barucija <barucijaenes@gmail.com>
2024-10-17 22:41:43 +0200

    map full screen

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 const map = L.map("map", { attributionControl: false }).setView(
   [38.2906, -0.00388395],
-  7
+  6
 );
 L.control.attribution({ prefix: false }).addTo(map);
 const tilesource_layer = L.tileLayer(
   "https://enes-barucija.github.io/dndTestMap/map/{z}/{x}/{y}.png",
   {
-    minZoom: 2,
+    minZoom: 5,
     maxZoom: 9,
     tms: false,
     attribution: "Created by QGIS",
